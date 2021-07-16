7e75402f25c8f1a81e49bd5503000e1ed8a01246
Evgen-r7 <73815378+Evgen-r7@users.noreply.github.com>
2021-07-16 15:29:23 +0300

    Update change_branches.bat

--- change_branches.bat
+++ change_branches.bat
@@ -1,54 +1,56 @@
-Vlads, [16.07.21 09:51]
-cd C:/r7build63
+set version=v6.3.1.70
+cd C:/de%version%
 
 git clone https://github.com/ONLYOFFICE/build_tools.git 
 cd build_tools
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/core.git
 cd core
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/desktop-apps.git
 cd desktop-apps
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/desktop-sdk.git
 cd desktop-sdk
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/dictionaries.git
 cd dictionaries
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/sdkjs.git
 cd sdkjs
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ONLYOFFICE/web-apps.git
 cd web-apps
-git checkout v6.3.1.70
+git checkout %version%
 
 cd ..
 
 git clone https://github.com/ASC-OFFICE/r7
+cd r7
+git checkout %version%
+
+cd ..
 
 git clone https://github.com/ONLYOFFICE/sdkjs-plugins.git
-# leave at master - last commits:
-  Commits on Sep 28, 2020
-  Update README.md (#129) вЂ¦
-  hellonadya committed on 28 Sep 2020
-  Merge branch 'hotfix/v5.5.4' вЂ¦
-  trofim24 committed on 3 Aug 2020
+cd sdkjs-plugins
+git checkout %version%
+
+cd ..
