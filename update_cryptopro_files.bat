dcf97709b9936e3a9684e1650fc759b2e5525c2b
Evgen-r7 <73815378+Evgen-r7@users.noreply.github.com>
2021-07-19 23:07:29 +0300

    Update update_cryptopro_files.bat

--- update_cryptopro_files.bat
+++ update_cryptopro_files.bat
@@ -1,5 +1,5 @@
-set cur_path=C:\de62
-set source_dir=%cur_path%\web-apps-cryptopro-branch-changes
+set cur_path=C:\documenteditors
+set source_dir=%cur_path%\web-apps-cryptopro
 set source_apps=%source_dir%\web-apps\apps\
 set target=%cur_path%\web-apps\apps\
 
@@ -10,6 +10,7 @@ copy /y %source_apps%documenteditor\main\app\view\CryptoProSign.js %target%docum
 copy /y %source_apps%documenteditor\main\app\view\CryptoProDialog.js %target%documenteditor\main\app\view\CryptoProDialog.js
 copy /y %source_apps%documenteditor\main\app\model\CryptoProModel.js %target%documenteditor\main\app\model\CryptoProModel.js
 
+copy /y %source_apps%documenteditor\main\app.js %target%documenteditor\main\app.js
 copy /y %source_apps%documenteditor\main\app\controller\Main.js %target%documenteditor\main\app\controller\Main.js
 copy /y %source_apps%common\main\lib\view\Protection.js %target%common\main\lib\view\Protection.js
 copy /y %source_apps%common\main\lib\controller\Protection.js %target%common\main\lib\controller\Protection.js
