328db3d801c62541ac222a46dfd9bc6034d8daaa
r7dmitry <Dmitry.Parhomenko@r7-office.ru>
2021-07-23 15:46:13 +0300

    adding automove ru.json file

--- update_cryptopro_files.bat
+++ update_cryptopro_files.bat
@@ -1,10 +1,13 @@
-set cur_path=C:\documenteditors
+set cur_path=C:\Desktopeditors\
 set source_dir=%cur_path%\web-apps-cryptopro
-set source_apps=%source_dir%\web-apps\apps\
 set target=%cur_path%\web-apps\apps\
+set source_apps=%source_dir%\web-apps\apps\
+
 
 cd %source_dir%
 git pull
+
+copy /y %source_apps%documenteditor\main\locale\ru.json %target%documenteditor\main\locale\ru.json
 
 copy /y %source_apps%documenteditor\main\app\view\CryptoProSign.js %target%documenteditor\main\app\view\CryptoProSign.js
 copy /y %source_apps%documenteditor\main\app\view\CryptoProDialog.js %target%documenteditor\main\app\view\CryptoProDialog.js
