eb4905579bee60b97e4942592c47c3d951e8587c
Evgen-r7 <evgeniy.ahmetgaraev@r7-office.ru>
2021-07-25 16:41:46 +0300

    added script update cryptopro files

---
+++ update_cryptopro_files.js
@@ -0,0 +1,89 @@
+const fs = require('fs'),
+    path = require('path'),
+    scandir = require('scandir'),
+    WEB_APPS = 'web-apps',
+    CRYPTOPRO = 'cryptopro',
+    { spawnSync } = require('child_process');
+
+const childProcess = spawnSync('git', ['pull']);
+
+console.log('******* command [git pull] output BEGIN *******');
+childProcess.output.forEach((e) => {
+    if (!e) return;
+    console.log(String(e));
+});
+console.log('******* command [git pull] output END *******');
+
+getFiles()
+    .then(copyFiles)
+    .catch((err) => {
+        console.error(err);
+    });
+
+function getFiles() {
+    return new Promise((resolve, reject) => {
+        let scan = scandir.create();
+        let files = [];
+
+        scan.on('file', (file, stats) => {
+            files.push(file);
+        });
+
+        scan.on('error', (err) => {
+            console.log('scandir err ', err);
+            reject(err);
+        });
+
+        scan.on('end', () => {
+            resolve(files);
+        });
+
+        scan.scan({
+            dir: `../${WEB_APPS}-${CRYPTOPRO}/${WEB_APPS}`,
+            recursive: true,
+        });
+    });
+}
+
+async function copyFiles(files) {
+    if (!Array.isArray(files) || files.length < 1) {
+        return console.error(`files is not Array ${files} or not found files`);
+    }
+
+    try {
+        const promises = files.map(
+            (e) =>
+                new Promise((resolve, reject) => {
+                    const source = path.resolve(e);
+                    const target = source.replace('\\' + WEB_APPS + '-' + CRYPTOPRO, '');
+
+                    function ensureDirectoryExistence(target) {
+                        var dirname = path.dirname(target);
+                        if (fs.existsSync(dirname)) {
+                            return true;
+                        }
+                        ensureDirectoryExistence(dirname);
+                        fs.mkdirSync(dirname);
+                    }
+
+                    ensureDirectoryExistence(target);
+
+                    fs.copyFile(source, target, (err) => {
+                        if (err) {
+                            console.error(`\n\rCopy error ${source} => ${target}`);
+                            return reject(err);
+                        }
+
+                        console.log(`\n\rCopy success ${source}
+             ${target}`);
+                        resolve();
+                    });
+                }),
+        );
+
+        await Promise.all(promises);
+        console.log(`\n\rSuccess copy ${promises.length} files!`);
+    } catch (e) {
+        console.error(e);
+    }
+}
